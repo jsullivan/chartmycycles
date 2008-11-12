ffc00ed18275ce0246daacda80bd2e8873f243de
Josh Sullivan <tumalo@gmail.com>
2008-11-11 18:11:31 -0800

    this is my second commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,23 @@
 set :application, "chartmycycles"
 # deploy_to : location of the application on the remote deployment server .
 # Create this folder before cap deploy.
-set :deploy_to, "/var/www/my_app"
+set :deploy_to, "/www/chartmycyles"
 set :scm, "git"
 set :repository,  "git@github.com:jsullivan/chartmycycles.git"
 set :branch, "master"
 default_run_options[:pty] = true
+set :use_sudo, false
 set :ssh_options, { :forward_agent => true }
 set :user, "josh"
 role :app, "dipperstove.com"
 role :web, "dipperstove.com"
 role :db,  "dipperstove.com", :primary => true
 set :deploy_via, :remote_cache
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, "passenger:restart"
